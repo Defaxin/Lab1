8c92718da581b73f2ab5216952d6dad5ad5f7459
andryxatt <vlandroid228@gmail.com>
2024-03-09 14:42:17 +0200

    Add project files.

---
+++ ConsoleApp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Specialized;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTask 1: ");
+            Work.Task1();
+            Console.WriteLine("\nTask 2: ");
+            Work.Task2();
+            Console.WriteLine("\nTask 3: ");
+            Work.Task3();
+            Console.WriteLine("\nTask 4: ");
+            Work.Task4();
+            Console.WriteLine("\nTask 5: ");
+            Work.Task5();
+            Console.WriteLine("\nTask 6: ");
+            Work.Task6();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace ConsoleApp1
+{
+    public static class Work
+    {
+        public static void Task1()
+        {
+            Console.WriteLine("It's easy\r\nto win forgiveness for being wrong; being right is what gets you\r\ninto real trouble.");
+        }
+        public static void Task2()
+        {
+            int sum = 0, r = 0, d = 1;
+            int max = 0, min = 99999;
+            int count = 5;
+            int a;
+            int[] numbers = new int[count];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                r = int.Parse(Console.ReadLine());
+                sum += r;
+                if (r > max)
+                {
+                    max = r;
+                }
+                if (r < min)
+                {
+                    min = r;
+                }
+                d *= r;
+            }
+            Console.WriteLine("Sum: ");
+            Console.WriteLine(sum);
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+            Console.WriteLine("Min: ");
+            Console.WriteLine(min);
+            Console.WriteLine("Product: ");
+            Console.WriteLine(d);
+        }
+        public static void Task3()
+        {
+            int s;
+            int c = 6;
+            int[] n = new int[c];
+            for (int i = 0; i < n.Length; i++)
+            {
+                s = int.Parse(Console.ReadLine());
+                n[i] = s;
+            }
+            n.Reverse();
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine(n[i]);
+            }
+        }
+        public static void Task4()
+        {
+            int a = 0, b = 1, s = 0;
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                a = n;
+            }
+            for (int i = n; n < m; n++)
+            {
+                a = a + b;
+                b++;
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+        }
+        public static void Task5()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a < b)
+            {
+                while (a < b)
+                {
+
+                    for (int i = 0; i < a; i++)
+                    {
+                        Console.Write(a);
+                    }
+                    a++;
+                    Console.WriteLine();
+                }
+            }
+        }
+        public static void Task6()
+        {
+            int l = int.Parse(Console.ReadLine());
+            char g = char.Parse(Console.ReadLine());
+            for (int i = 0; i < l; i++)
+            {
+                Console.Write(g);
+            }
+        }
+    }
+}
